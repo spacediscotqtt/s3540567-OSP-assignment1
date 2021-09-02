5ff5abbd615acfe9a690333ae1bc86dbe6981825
spacediscotqtt <s3540567@student.rmit.edu.au>
2021-09-02 18:52:21 +1000

    add B

---
+++ B.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <semaphore.h>
+#include <pthread.h>
+#include <unistd.h>
+
+#define NUMTHREAD 5
+
+enum state{
+ EATING , HUNGRY , THINKING 
+};
+
+int main(){
+
+return 0;
+}
+
+/*do{
+
+
+
+}*/
