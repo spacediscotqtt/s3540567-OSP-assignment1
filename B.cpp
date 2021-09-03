9e54f54fd5f4d0b867c5570490a5434499efbda7
spacediscotqtt <s3540567@student.rmit.edu.au>
2021-09-03 11:43:04 +1000

    added agent method

--- B.cpp
+++ B.cpp
@@ -1,3 +1,5 @@
+//solution was derived from the psuedo code provided by http://www.cs.umd.edu/~hollings/cs412/s96/synch/smokers.html
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,28 +8,43 @@
 #include <unistd.h>
 #include <iostream>
 
-#define NUMTHREAD 5
+#define SMOKERS 3
+#define EMPTY 0
 
-void ingredients(int placement){
-    switch(placement){
-        case 1:
+int table = 0;
+pthread_mutex_t tableMutex;
+pthread_cond_t fullTable;
+pthread_cond_t emptyTable;
+
+void* agent(void* arg)
+{
+    for(int i = 0; i < 3; i++){
+        pthread_mutex_lock(&tableMutex);
+        if(table == EMPTY)
+        {
+            //Fills table and waits for empty signal
+            table = rand()%3 + 1;
+            switch(table){
+              case 1:
             std::cout << "Tobacco and Matches" << std::endl;
             break;
-        case 2:
+              case 2:
             std::cout << "Paper and Matches" << std::endl;
             break;
-        case 3:
+              case 3:
             std::cout << "Paper and Tobacco" << std::endl;
     }
+            pthread_cond_signal(&fullTable);
+            pthread_cond_wait(&emptyTable, &tableMutex);
+        }
+        pthread_mutex_unlock(&tableMutex);
+    }
+    return NULL;
 }
 
 int main(){
+     pthread_t agent, smokers[SMOKERS];
 
 return 0;
 }
 
-/*do{
-
-
-
-}*/
